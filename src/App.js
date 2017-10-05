6421d7418abe3bd91846f8e9efe3c6bb634a448a
vetsinen <vetsinen@gmail.com>
2017-10-05 14:34:45 +0300

    items rendered manually

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    renderItem(i) {
+        return (
+            <div>item<strong>{i}</strong></div>
+        )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                {this.renderItem(3)}
+                {this.renderItem(4)}
+
+            </div>
+        );
+    }
 }
 
 export default App;
